16396d67403af31226a6fdd31f53f2317853fe1c
Frederic Lamsens <frederic.lamsens@ugent.be>
2025-11-18 15:11:43 +0000

    update build script

---
+++ build_manifests.py
@@ -0,0 +1,346 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import json
+import shutil
+
+from iiif_prezi3 import Manifest, AnnotationPageRef, ResourceItem, config, Canvas, ExternalItem, Collection
+
+import helpers.annotations as annotations
+import helpers.cuneur as cuneur
+import helpers.svg as svg
+from helpers.atf_indexer import AtfIndexer
+
+from helpers.iiif_resources import create_canvas_with_choice, create_image_resource_item
+from helpers.iiif_uri_helper import IfffUriHelper
+from helpers.nodegoat_metadata import nodegoat_to_iiif_metadata
+from helpers.resources import scan_images, scan_manifests
+from config import _config
+
+config.configs['helpers.auto_fields.AutoLang'].auto_lang = "en"
+
+# init uri
+base_path = _config['base_path']
+
+# init url/path helper
+uri_helper = IfffUriHelper(_config['base_url'], _config['base_path'])
+
+# find manifest folders
+manifests = scan_manifests(base_path)
+# manifests = ['/workspaces/data/O.0219'] 
+
+# create atf reader
+atf_reader = AtfIndexer()
+
+# create collection manifest
+collection_manifest = Collection(id=uri_helper.create_collection_uri(_config['collection_id']), label="Cune-iiif-orm SDE",items=[], type="Collection")
+
+# loop over manifests
+for manifest_path in manifests:
+
+    tablet_id = os.path.basename(manifest_path)
+
+    print(f"Processing {tablet_id} ...")
+
+    # clean previous exports
+    print("- Cleaning previous exports ...")
+    manifest_paths = [
+        os.path.join(manifest_path, "manifest.json"),
+        os.path.join(manifest_path, "sign-annotations.json"),
+        os.path.join(manifest_path, "sign-annotations-new.json"),
+        os.path.join(manifest_path, f"{tablet_id}-sign-annotations.json"),
+        os.path.join(manifest_path, f"{tablet_id}-word-annotations.json"),
+        os.path.join(manifest_path, f"{tablet_id}-line-annotations.json"),
+        os.path.join(manifest_path, f"{tablet_id}-translation-annotations.json"),
+        os.path.join(manifest_path, f"{tablet_id}-translations.json"),
+        os.path.join(manifest_path, f"{tablet_id}-transliteration-annotations.json"),
+        os.path.join(manifest_path, f"{tablet_id}-transliterations.json"),
+    ]
+    for path in manifest_paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+    # create annotation-page and annotation directory
+    manifest_paths = {
+        "annotation-page": os.path.join(manifest_path, "annotation-page"),
+        "annotation": os.path.join(manifest_path, "annotation"),
+        "data": os.path.join(manifest_path, "data")
+    }
+
+    # create and empty folders defined in paths
+    print("- Create output directories ...")
+    for key, path in manifest_paths.items():
+        # create directory
+        if not os.path.exists(path):
+            os.mkdir(path)
+        # empty directory
+        for filename in os.listdir(path):
+            file_path = os.path.join(path, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+    # scan images
+    images = scan_images(manifest_path)
+
+    # load nodegoat data
+    print("- Load nodegoat data ...")
+    nodegoat_data = {}
+    nodegoat_data_path = os.path.join(manifest_path, f"{tablet_id}-data.json")
+    if os.path.exists(nodegoat_data_path):
+        with open(nodegoat_data_path, 'r', encoding='utf-8') as f:
+            nodegoat_data = json.load(f)
+
+        # copy file to data folder
+    else:
+        print(f"- No nodegoat data found at {nodegoat_data_path}")
+
+    # load transliteration
+    print("- Load transliteration ...")
+    transliteration_path = os.path.join(manifest_path, f"{tablet_id}-transliteration-atf.txt")
+    transliteration_text = None
+    if os.path.exists(transliteration_path):
+        with open(transliteration_path, 'r', encoding='utf-8') as f:
+            transliteration_text = f.read()
+    else:
+        print(f"- No transliteration found at {transliteration_path}")
+
+    # Load translations
+    print("- Load translation ...")
+    translation_data_path = os.path.join(manifest_path, f"{tablet_id}-translation.txt")
+    if os.path.exists(translation_data_path):
+        translation_text = open(translation_data_path, 'r', encoding='utf-8').read()
+    else:
+        print(f"- No translation found at {translation_data_path}")
+
+    # load sign annotations
+    print("- Load sign annotations ...")
+    annotations_source_path = os.path.join(manifest_path, f"{tablet_id}-annotator-annotations.json")
+    sign_data_list = []
+    if os.path.exists(annotations_source_path):
+        sign_data_list = cuneur.parse_cuneur_annotations(annotations_source_path)
+    else:
+        print(f"- No sign annotations found at {annotations_source_path}")
+
+    # add missing atf word index to sign annotations
+    print("- Add missing word indexes to sign annotations ...")
+    if transliteration_text:           
+        try:
+            atf_reader.set_text(transliteration_text)
+
+            for annotation in sign_data_list:
+                if 'word_index' not in annotation:
+                    char_info = atf_reader.get_char_info(annotation['side'], annotation['line_index'], annotation['char_index'])
+                    if char_info:
+                        annotation['word_index'] = char_info['word_index']
+                    else:
+                        print(f"Warning: no char info for {annotation['side']} {annotation['line_index']} {annotation['char_index']}")
+
+        except Exception as e:
+            print(f"- Could not parse {transliteration_path}")
+            continue
+
+    # create resource images
+    image_resource_items: list[ResourceItem] = [create_image_resource_item(image, uri_helper, _config) for image in images]
+
+    # init manifest
+    manifest_id = _config["manifest_id_prefix"] + uri_helper.id_from_path(manifest_path + '/')
+    manifest_uri = uri_helper.create_manifest_uri(manifest_id)
+    manifest_label = os.path.basename(manifest_path)
+    print(f"Creating manifest: {manifest_id}")
+   
+    manifest = Manifest(
+        id=manifest_uri,
+        label=manifest_label,
+        **{
+            "@context": None
+        }
+    )
+        
+    # add thumbnail
+    thumbnail_id = str(image_resource_items[0].service[0].id) + "/full/200,/0/default.jpg"
+    thumbnail_resource = ResourceItem(id=thumbnail_id, type="Image", width=100, height=50, format="image/jpeg")
+    thumbnail_resource.service = image_resource_items[0].service
+
+    manifest.thumbnail = [ thumbnail_resource ]
+
+    # add metadata
+    if len(nodegoat_data):
+        metadata = nodegoat_to_iiif_metadata(nodegoat_data)
+        manifest.metadata = metadata
+
+    # choice? create single canvas, annotationpage, annotation with body choice target canvas, annotation contains image resource items
+    canvases: list[Canvas] = []
+
+    # add single canvas with choice
+    # use manifest label as canvas label
+    canvas_id = '0001'
+    canvas = create_canvas_with_choice(canvas_id, manifest.label, manifest_id, image_resource_items, uri_helper)   
+    canvases.append(canvas)
+    canvas_uri = canvas.id
+
+    # add translation
+    if translation_text:
+
+        # create annotation
+        annotation_uri = uri_helper.create_manifest_annotation_uri(manifest_id, f"{tablet_id}-translation.json")
+        annotation_path = uri_helper.create_manifest_annotation_path(tablet_id, f"{tablet_id}-translation.json")
+
+        annotation = annotations.create_text_annotation(
+            **{
+                "id": annotation_uri,
+                "text": translation_text,
+                "format": "text/plain",
+                "purpose": "translating",
+                "motivation": "describing",
+                "target": canvas.id
+            }            
+        )
+        annotations.save_iiif_model(annotation, annotation_path)
+
+        # create translation annotation page
+        anno_page_uri = uri_helper.create_manifest_annotation_page_uri(manifest_id, f"{tablet_id}-translations.json")
+        anno_page_path = uri_helper.create_manifest_annotation_page_path(tablet_id, f"{tablet_id}-translations.json")
+
+        anno_page = annotations.create_annotation_page(anno_page_uri, "Translations", [annotation])       
+        annotations.save_iiif_model(anno_page, anno_page_path)
+
+        # add annotation page reference to canvas
+        anno_page_ref = AnnotationPageRef(
+            id=anno_page.id,
+            type="AnnotationPage",
+        )
+        canvas.annotations.append(anno_page_ref)
+
+    # add transliteration
+    if transliteration_text:
+
+        # create annotation
+        annotation_uri = uri_helper.create_manifest_annotation_uri(manifest_id, f"{tablet_id}-transliteration.json")
+        annotation_path = uri_helper.create_manifest_annotation_path(tablet_id, f"{tablet_id}-transliteration.json")
+
+        annotation = annotations.create_text_annotation(
+            **{
+                "id": annotation_uri,
+                "text": transliteration_text,
+                "format": "text/x-atf",
+                "purpose": "transliterating",
+                "motivation": "describing",
+                "target": canvas.id
+            }            
+        )
+        annotations.save_iiif_model(annotation, annotation_path)
+
+        # create annotation page
+        anno_page_uri = uri_helper.create_manifest_annotation_page_uri(manifest_id, f"{tablet_id}-transliterations.json")
+        anno_page_path = uri_helper.create_manifest_annotation_page_path(tablet_id, f"{tablet_id}-transliterations.json")        
+
+        anno_page = annotations.create_annotation_page(anno_page_uri, "Transliterations", [annotation])
+        annotations.save_iiif_model(anno_page, anno_page_path)
+
+        # add annotation page reference to canvas
+        anno_page_ref = AnnotationPageRef(
+            id=anno_page.id,
+            type="AnnotationPage",
+        )
+        canvas.annotations.append(anno_page_ref)
+
+    # add sign annotations?
+    if len(sign_data_list):
+
+        # rescale sign annotations
+        scale_factor = max([canvas.width/_config['image_api']['max_width'], canvas.height/_config['image_api']['max_height']])
+        if scale_factor != 1:
+            for annotatation_index, annotation in enumerate(sign_data_list):
+                annotation['points'] = svg.rescale_points(annotation['points'], scale_factor)
+
+        # create annotations
+        items = []
+        for sign in sign_data_list:
+            annotation_uri = uri_helper.create_manifest_annotation_uri(manifest_id, f"{sign['id']}.json")
+            annotation_path = uri_helper.create_manifest_annotation_path(tablet_id, f"{sign['id']}.json")
+
+            annotation = annotations.create_sign_annotation(sign, annotation_uri, canvas.id)
+            annotations.save_iiif_model(annotation, annotation_path)
+
+            items.append(annotation)
+
+        # create annotation page
+        anno_page_uri = uri_helper.create_manifest_annotation_page_uri(manifest_id, f"{tablet_id}-signs.json")
+        anno_page_path = uri_helper.create_manifest_annotation_page_path(tablet_id, f"{tablet_id}-signs.json")
+
+        anno_page = annotations.create_annotation_page(anno_page_uri, "Sign Annotations", items)
+        annotations.save_iiif_model(anno_page, anno_page_path)
+
+        # add annotation page reference to canvas
+        annotation_page_ref = AnnotationPageRef(
+            # id=uri_helper.create_canvas_annotation_page_uri(manifest_id, canvas_id, "sign-annotations"),
+            id=anno_page.id,
+            type="AnnotationPage",
+        )
+        canvas.annotations.append(annotation_page_ref)
+
+    # add word annotations?
+
+    # add line annotations            
+
+    # add canvases to manifest
+    manifest.items = canvases
+
+    # add seeAlso
+    data_files = [
+        {
+            "file": f"{tablet_id}-transliteration-atf.txt",
+            "label": "Transliteration (ATF)",
+            "type": "text/plain",
+        },
+        {
+            "file": f"{tablet_id}-translation.txt",
+            "label": "Translation",
+            "type": "text/plain",
+        },
+        {
+            "file": f"{tablet_id}-data.json",
+            "label": "Metadata",
+            "type": "application/json",
+        },
+    ]
+
+    see_also = []
+    for data_file in data_files:
+        data_file_uri = uri_helper.create_manifest_data_uri(manifest_id, data_file['file'])
+        data_file_path = uri_helper.create_manifest_data_path(tablet_id, data_file['file'])
+
+        if os.path.exists(os.path.join(manifest_path, data_file['file'])):
+            # copy file to data path
+            shutil.copy2(os.path.join(manifest_path, data_file['file']), data_file_path)
+
+        item = ExternalItem(
+            id=data_file_uri,
+            type="Dataset",
+            label=data_file['label'],
+            format=data_file['type'],
+            profile="https://iiif.io/api/presentation/3/seeAlso.json"
+        )
+        see_also.append(item)
+    manifest.seeAlso = see_also
+
+    # output manifest
+    annotations.save_iiif_model(manifest, os.path.join(manifest_path, 'manifest.json'))
+
+    # add to collection manifest
+    manifest_ref = Manifest(
+        id=manifest.id,
+        label=manifest.label,
+        **{
+            "@context": None
+        }
+        )
+    manifest_ref.thumbnail = manifest.thumbnail
+    collection_manifest.items.append(manifest_ref)
+
+
+# output collection manifest
+collection_manifest_uri = uri_helper.create_collection_uri(_config['collection_id'])
+collection_manifest_path = os.path.join(base_path, 'collection.json')
+annotations.save_iiif_model(collection_manifest, collection_manifest_path)
+
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+_config = {
+    "base_url": "https://iiif.ghentcdh.ugent.be/iiif",
+    "base_path": os.path.abspath('/workspaces/data'),
+    "image_api": {
+        "type": "ImageService2",
+        "profile": "level2",
+        "max_width": 5000,
+        "max_height": 5000
+    },
+    "image_id_prefix": "cune-iiif-orm:testset:",
+    "manifest_id_prefix": "cune-iiif-orm:sde:",
+    "collection_id": "cune-iiif-orm:sde"
+}
